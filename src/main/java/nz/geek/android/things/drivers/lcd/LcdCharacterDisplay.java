e45a02e3b4c21ebe1c57fc44982e0a90ef65bc08
Dave McKelvie <dave@subverse.co.nz>
2017-09-14 20:13:02 +1200

    fleshing out gpio version

--- src/main/java/nz/geek/android/things/drivers/lcd/LcdCharacterDisplay.java
+++ src/main/java/nz/geek/android/things/drivers/lcd/LcdCharacterDisplay.java
@@ -2,41 +2,64 @@ package nz.geek.android.things.drivers.lcd;
 
 import com.google.android.things.pio.Gpio;
 
+import java.io.IOException;
+
 
 public class LcdCharacterDisplay extends AbstractCharacterDisplay {
 
-  private final Gpio e1Pin;
-  private final Gpio e2Pin;
-  private final Gpio rsPin;
-  private final Gpio rwPin;
-  private final Gpio blPin;
-  private final Gpio d4Pin;
-  private final Gpio d5Pin;
-  private final Gpio d6Pin;
-  private final Gpio d7Pin;
+  private Gpio ePin;
+
+  private static final int E1 = 0;
+  private static final int E2 = 1;
+  private static final int RS = 2;
+  private static final int RW = 3;
+  private static final int D4 = 4;
+  private static final int D5 = 5;
+  private static final int D6 = 6;
+  private static final int D7 = 7;
+  private static final int BL = 8;
+  private Gpio[] gpios = new Gpio[9];
 
   private LcdCharacterDisplay(int width, int height, Gpio e1Pin, Gpio e2Pin, Gpio rsPin, Gpio rwPin,
                               Gpio blPin, Gpio d4Pin, Gpio d5Pin, Gpio d6Pin, Gpio d7Pin, boolean hasBackLight) {
     super(width, height, hasBackLight);
-    this.e1Pin = e1Pin;
-    this.e2Pin = e2Pin;
-    this.rsPin = rsPin;
-    this.rwPin = rwPin;
-    this.blPin = blPin;
-    this.d4Pin = d4Pin;
-    this.d5Pin = d5Pin;
-    this.d6Pin = d6Pin;
-    this.d7Pin = d7Pin;
+    gpios[E1] = e1Pin;
+    gpios[E2] = e2Pin;
+    gpios[RS] = rsPin;
+    gpios[RW] = rwPin;
+    gpios[BL] = blPin;
+    gpios[D4] = d4Pin;
+    gpios[D5] = d5Pin;
+    gpios[D6] = d6Pin;
+    gpios[D7] = d7Pin;
+    ePin = e1Pin;
   }
 
   @Override
   public void connect() {
-
+    for (Gpio gpio : gpios) {
+      if (gpio != null) {
+        try {
+          gpio.setDirection(Gpio.DIRECTION_OUT_INITIALLY_HIGH);
+          gpio.setActiveType(Gpio.ACTIVE_HIGH);
+        } catch (IOException e) {
+          //e.printStackTrace();
+        }
+      }
+    }
   }
 
   @Override
   public void disconnect() {
-
+    for (Gpio gpio : gpios) {
+      if (gpio != null) {
+        try {
+          gpio.close();
+        } catch (IOException e) {
+          //
+        }
+      }
+    }
   }
 
   @Override
@@ -45,58 +68,79 @@ public class LcdCharacterDisplay extends AbstractCharacterDisplay {
   }
 
   @Override
-  public void print(int line, String message) {
-
-  }
-
-  @Override
-  public void clearLine(int line) {
-
-  }
-
-  @Override
-  public void clearDisplay() {
-
+  protected void switchDisplay(int display) {
+    if (display == 2) {
+      ePin = gpios[E2];
+    } else {
+      ePin = gpios[E1];
+    }
   }
 
   @Override
   public void enableBackLight(boolean enable) {
-
+    if (hasBackLight) {
+      try {
+        gpios[BL].setValue(enable);
+      } catch (IOException e) {
+        //
+      }
+    }
   }
 
-  @Override
-  public boolean hasBackLight() {
-    return false;
+  private void writeData(int data) {
+    try {
+      gpios[D7].setValue(((data & 0x80) == 0x80));
+      gpios[D6].setValue(((data & 0x40) == 0x40));
+      gpios[D5].setValue(((data & 0x20) == 0x20));
+      gpios[D4].setValue(((data & 0x10) == 0x10));
+    } catch (IOException e) {
+      //
+    }
   }
-
   @Override
   protected void write(int data) {
+    try {
+      // RS = rs, E = 0, R/W = 0
+      ePin.setValue(false);
+      gpios[RW].setValue(false);
 
+      // RS = rs, E = 1, R/W = 0
+      ePin.setValue(true);
+
+      // D0-7 = data
+      writeData(data);
+
+      // RS = rs, E = 0, R/W = 0
+      ePin.setValue(false);
+
+      if (doubleWrite) {
+        // RS = rs, E = 1, R/W = 0
+        ePin.setValue(true);
+
+        writeData(data << 4);
+
+        // RS = rs, E = 0, R/W = 0
+        ePin.setValue(false);
+      }
+    } catch (IOException e) {
+      //
+    }
   }
 
   @Override
   protected void writeCommand(int command) {
+    try {
+      gpios[RS].setValue(false);
+      write(command);
+      gpios[RS].setValue(true);
+    } catch (IOException e) {
+      //
+    }
 
   }
 
-  @Override
-  protected void switchDisplay(int display) {
-
-  }
-
-  @Override
-  public void setCustomCharacter(int address, byte[] pattern) {
-
-  }
-
-  @Override
-  public int setCustomCharacter(byte[] pattern) {
-    return 0;
-  }
-
-  @Override
-  public void initBarGraph() {
-
+  public static LcdCharacterDisplay.Builder builder(int width, int height) {
+    return new Builder(width, height);
   }
 
   public static final class Builder{
@@ -115,7 +159,7 @@ public class LcdCharacterDisplay extends AbstractCharacterDisplay {
     private Gpio d7Pin;
 
 
-    public Builder(int width, int height) {
+    private Builder(int width, int height) {
       this.width = width;
       this.height = height;
     }
